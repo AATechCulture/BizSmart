a4ae62c2e83de3ed232a6c292db9e704a16bf326
= <aleclair.1105@outlook.com>
2024-11-15 22:37:29 -0600

    newfakenewReal

--- py/fake.py
+++ py/fake.py
@@ -18,27 +18,29 @@ db = teradatasql.connect(host=HOST,user = USER, password = PASS)
 curse = db.cursor()
 
 
-curse.execute("""
-              CREATE SET TABLE {USER}.Transactionals(
-              business_id INTEGER,
-              business_name VARCHAR(255), business_type VARCHAR(255), 
-              location VARCHAR(255), owner_name VARCHAR(255), 
-              transaction_date TIMESTAMP, 
-              transaction_amount FLOAT(4), 
-              category VARCHAR(255), 
-              payment_method VARCHAR(255), 
-              vendor_name VARCHAR(255), 
-              vendor_location VARCHAR(255),
-              UNIQUE PRIMARY INDEX(business_id)
+# curse.execute(f"""
+#               CREATE TABLE {USER}.Transactionals(
+#               business_id INTEGER,
+#               business_name VARCHAR(255), 
+#               business_type VARCHAR(255), 
+#               location VARCHAR(255), 
+#               owner_name VARCHAR(255), 
+#               transaction_date TIMESTAMP, 
+#               transaction_amount FLOAT(4), 
+#               category VARCHAR(255), 
+#               payment_method VARCHAR(255), 
+#               vendor_name VARCHAR(255), 
+#               vendor_location VARCHAR(255)
 
-              )
+#               ) PRIMARY INDEX (business_id)
               
-              """
-              )
+#               """
+#               )
 
 
 
 # Generate the data
+i = 0
 data = [4]
 for _ in range(num_sets):
         business_name = faker.company()
@@ -51,9 +53,10 @@ for _ in range(num_sets):
         payment_method = faker.random_element(["Credit Card", "Cash", "Bank Transfer"])
         vendor_name = faker.company()
         vendor_location = faker.city()
-        query = "INSERT INTO {USER}.Transactionals (business_name, business_type, location, owner_name, transaction_date, transaction_amount, category, payment_method, vendor_name, vendor_location) VALUES (?,?,?,?,?,?,?,?,?,?)"
-        values = (business_name, business_type, location, owner_name, transaction_date, transaction_amount, category, payment_method, vendor_name, vendor_location)
-        curse.executemany(query, values)
+        query = f"INSERT INTO {USER}.Transactionals (business_id, business_name, business_type, location, owner_name, transaction_date, transaction_amount, category, payment_method, vendor_name, vendor_location) VALUES (?,?,?,?,?,?,?,?,?,?,?)"
+        values = (i, business_name, business_type, location, owner_name, transaction_date, transaction_amount, category, payment_method, vendor_name, vendor_location)
+        curse.execute(query, values)
+        i+=1
 
 # Print the data
 print(data)
@@ -69,43 +72,3 @@ curse.close()
 
 
 
-import teradatasql
-
-DB_URL = ""                                 #Add Host
-USER = ""                                   #Add Username
-PASS = ""                                   #Add Password
-
-try:
-    # Establish a connection to the Teradata database
-    with teradatasql.connect(host=DB_URL, user=USER, password=PASS) as con:
-        # Create a cursor to execute queries
-        with con.cursor() as cur:
-            try:
-                # Creating the table SampleEmployee
-           
-                
-                print(f"Sample table {USER}.SampleEmployee created.")
-
-                # Adding sample data into SampleEmployee table
-                cur.execute (f"INSERT INTO {USER}.SampleEmployee VALUES (1, 'Richard Hendricks','CEO')")
-                cur.execute (f"INSERT INTO {USER}.SampleEmployee VALUES (2, 'Jared Dunn','CFO')")
-                cur.execute (f"INSERT INTO {USER}.SampleEmployee VALUES (3, 'Jian Yang','Intern')")
-
-                print(f"Sample data added to table {USER}.SampleEmployee.")
-
-                # Execute the SELECT query to get the results from SampleEmployee table 
-                cur.execute(f"SELECT * FROM {USER}.SampleEmployee")
-
-                # Extract data from the result set and print it
-                for row in cur:
-                    print(f"Associate ID: {row[0]}, Associate_Name: {row[1]}, Job_Title:{row[2]}")
-
-                
-
-            except teradatasql.DatabaseError as db_err:
-                # Handle any errors that occur during query execution
-                print("Error while executing the query:", db_err)
-
-except teradatasql.DatabaseError as db_err:
-    # Handle any errors that occur during the database connection
-    print("Error while connecting to the Teradata database:", db_err)
